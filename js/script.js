0ed40757965b13218449bd0a450c4a7e14719b3f
Logan Sage <lvdsage@gmail.com>
2020-11-25 21:35:43 -0700

    layout changes

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var cityInput = $(".selectCity");
 var parkCard = $(".parksData");
 
 
-var covidDataContainer = $(".covidDataContainer");
+var covidDataContainer = $(".something");
 var usCovidData = $(".usCovidInfo")
 // var searchForm = $(".selectStateForm");
 // var activitySelector = $(".selectActivityForm")
@@ -176,7 +176,9 @@ function getGoogleInfo(){
         };
         var parkN = $("<h1>");
         parkN.text(parkName)
-        var parkImg = $("<img>");
+        parkN.addClass("card-title")
+        var parkImg = $("<img> <br>");
+        parkImg.addClass("card-image")
         parkImg.attr("src",parkPic).attr("alt",`Picture of ${parkName}`)
         var parkLink = $("<a>");
         parkLink.attr("href",parkURL).attr("target","_blank")
